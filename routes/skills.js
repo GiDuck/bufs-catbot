d2e34179e7bf8411488f6d5e8405059563108844
Gi Deok Kim <gdtbgl93@gmail.com>
2019-12-24 19:24:19 +0900

    request test

--- routes/skills.js
+++ routes/skills.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/home', function(req, res) {
 
-  console.log(req.param);
+  console.log(req.params);
   const template = new TemplateBuilder()
   .simpleText("안녕 난 냥냥봇이야")
   .build();
