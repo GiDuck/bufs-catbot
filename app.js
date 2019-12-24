8659f43137560d1a5c5fe32ddfcdd809406745d1
Gi Deok Kim <gdtbgl93@gmail.com>
2019-12-24 16:29:20 +0900

    라우터명 변경

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var skillsRouter = require('./routes/skills');
 
 require('dotenv').config();
 
@@ -34,7 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', skillsRouters);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/skills.js
@@ -0,0 +1,23 @@
+const {TemplateBuilder, getResponseModel} = require ("../models/response.js"); 
+
+var express = require('express');
+var router = express.Router();
+
+
+/* GET response test */
+router.get('/', function(req, res, next) {
+  res.send('respond success');
+});
+
+router.post('/home', function(req, res) {
+
+  console.log(req);
+  const template = new TemplateBuilder()
+  .simpleText("안녕 난 냥냥봇이야")
+  .build();
+
+  res.status(200).send(getResponseModel(template));
+});
+
+
+module.exports = router;
