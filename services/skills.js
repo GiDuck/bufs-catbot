538fcf52d3734b5fc3ccabe7028f485962811c06
Gi Deok Kim <gdtbgl93@gmail.com>
2020-01-27 22:12:34 +0900

    예외처리

--- services/skills.js
+++ services/skills.js
@@ -3,6 +3,8 @@ const { Info }  = require ("../models/info.js");
 const getColleges = () => {
     Info.findAll({parent: "학과정보찾기"}).then(colleges=>{
         console.log(colleges);
+    }).catch(err => {
+        console.log(err);
     });
 }
 
