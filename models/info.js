448145ec248985bef1248e6011bfb19e05b97fe1
Gi Deok Kim <gdtbgl93@gmail.com>
2020-01-27 22:45:38 +0900

    collection 오탈자 수정

--- models/info.js
+++ models/info.js
@@ -4,7 +4,7 @@ const InfoSchema = new mongoose.Schema({
   name: { type: String, required: true },
   value: { type: Array, required: true },
   isTerminal: { type: Boolean, required: false }
-}, { collection: "catDB"} );
+}, { collection: "catDb"} );
 
 const Info = mongoose.model("Info", InfoSchema);
 
