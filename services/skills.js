96e73f2742d55d89eba6ac79a9c931fe175b4be4
Gi Deok Kim <gdtbgl93@gmail.com>
2020-01-27 22:41:19 +0900

    function 수정

--- services/skills.js
+++ services/skills.js
@@ -2,11 +2,11 @@ const { Info }  = require ("../models/info.js");
 
 const getColleges = (req, res) => {
     console.log("getColleges")
-    Info.find({parent: "학과정보찾기"}.then (colleges=>{
+    Info.find({parent: "학과정보찾기"}).then(colleges=>{
         console.log("학과정보찾기")
         console.log(err);
         console.log(colleges);
-    }));
+    });
 }
 
 module.exports =  ({ getColleges });
