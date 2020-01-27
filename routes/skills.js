a71426ba4c11578355590c7e6ec1cdadaff6be8b
Gi Deok Kim <gdtbgl93@gmail.com>
2020-01-28 00:08:19 +0900

    response 수정

--- routes/skills.js
+++ routes/skills.js
@@ -1,55 +1,42 @@
-const { TemplateBuilder, getResponseModel } = require ("../models/response.js"); 
-const { getColleges } = require ("../services/skills.js"); 
+const { TemplateBuilder, getResponseModel } = require("../models/response.js");
+const { getColleges } = require("../services/skills.js");
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-
 /* GET response test */
-router.get('/', function(req, res, next) {
-  res.send('respond success');
+router.get("/", function(req, res, next) {
+  res.send("respond success");
 });
 
-router.post('/buttons', function(req, res) {
-
+router.post("/buttons", function(req, res) {
   const template = new TemplateBuilder()
-  .simpleText("안녕 난 냥냥봇이야")
-  .build();
+    .simpleText("안녕 난 냥냥봇이야")
+    .build();
 
   res.status(200).send(getResponseModel(template));
 });
 
-
-router.post('/colleges', async (req, res) => {
-  
-  
+router.post("/colleges", async (req, res) => {
   console.log(JSON.stringify(req.params));
   const findData = await getColleges();
-  const colleages = findData.map(data=> data.name);
+  const colleages = findData.map(data => data.name);
   console.log("colleages", colleages);
 
+  const templateBuilder = new TemplateBuilder().simpleText(
+    "단과대학 목록이라냥"
+  );
 
-  const templateBuilder = new TemplateBuilder()
-  .simpleText("단과대학 목록이라냥");
-
-  colleages.forEach(colleage=>{
-    templateBuilder.setQuickReplies(
-      {
-        label: colleage,
-        action: "message",
-        messageText: colleage,
-      }
-    )
+  colleages.forEach(colleage => {
+    templateBuilder.setQuickReplies(colleage, "message", colleage);
   });
   const template = templateBuilder.build();
   res.status(200).send(getResponseModel(template));
 });
 
-router.post('/departments', function(req, res) {
-  
+router.post("/departments", function(req, res) {
   console.log(JSON.stringify(req.params));
   //res.status(200).send();
-
 
   // const template = new TemplateBuilder()
   // .simpleText("안녕 난 냥냥봇이야")
@@ -58,12 +45,9 @@ router.post('/departments', function(req, res) {
   // res.status(200).send(getResponseModel(template));
 });
 
-
-router.post('/department', function(req, res) {
-  
+router.post("/department", function(req, res) {
   console.log(JSON.stringify(req.params));
   //res.status(200).send();
-
 
   // const template = new TemplateBuilder()
   // .simpleText("안녕 난 냥냥봇이야")
@@ -71,7 +55,5 @@ router.post('/department', function(req, res) {
 
   // res.status(200).send(getResponseModel(template));
 });
-
-
 
 module.exports = router;
