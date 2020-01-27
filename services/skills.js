165bc901e290cdd4391c873fce9f0d7221b4bd97
Gi Deok Kim <gdtbgl93@gmail.com>
2020-01-27 22:21:14 +0900

    query 수정

--- services/skills.js
+++ services/skills.js
@@ -2,7 +2,7 @@ const { Info }  = require ("../models/info.js");
 
 const getColleges = () => {
     console.log("getColleges")
-    Info.findAll({parent: "학과정보찾기"}).then(colleges=>{
+    Info.find({parent: "학과정보찾기"}).then(colleges=>{
         console.log(colleges);
     }).catch(err => {
         console.log("error", err);
