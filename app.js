def919f5495128d46d15081d03bab06f648f0b0f
Gi Deok Kim <gdtbgl93@gmail.com>
2020-03-10 18:49:44 +0900

    fs import

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var fs = require('fs');
 
 var logger = require('morgan');
 
