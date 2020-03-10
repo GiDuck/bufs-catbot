8ba3e6df458433a2907d2809531b21f15dd87acb
Gi Deok Kim <gdtbgl93@gmail.com>
2020-03-11 00:15:47 +0900

    스타터 파일 생성

---
+++ app.starter.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+    apps: [
+        {
+            name: "BUFS-CATBOT",
+            script: "./bin/www",
+            watch: true,
+            env: {
+                "NODE_ENV": "development"
+            },
+            env_production: {
+                "NODE_ENV": "production"
+            }
+        }
+    ]
+}
